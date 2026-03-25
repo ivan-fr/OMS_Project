fffcb3213d4b207a57512ea0eea8c6d7c8529df1
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2026-03-25 12:40:03 +0100

    Ajout de la fonctionnalité Register avec validation et documentation Swagger

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+type CreateUserInput = {
+	email: string;
+	passwordHash: string;
+};
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	findByEmail(email: string) {
+		return this.prisma.user.findUnique({ where: { email } });
+	}
+
+	create(input: CreateUserInput) {
+		return this.prisma.user.create({
+			data: {
+				email: input.email,
+				passwordHash: input.passwordHash,
+			},
+			select: {
+				id: true,
+				email: true,
+				createdAt: true,
+			},
+		});
+	}
+}
