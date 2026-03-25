6205010179cbe6ca3af4e71540d4720c2693289c
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2026-03-25 15:13:17 +0100

    "[feat/US06]:choisir le trigger d’un workflow"

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,16 +5,18 @@ type CreateUserInput = {
 	email: string;
 	passwordHash: string;
 };
+import {EventEmitter2} from '@nestjs/event-emitter';
 
 @Injectable()
 export class UsersService {
-	constructor(private readonly prisma: PrismaService) {}
+	constructor(private readonly prisma: PrismaService, private eventEmitter: EventEmitter2) {}
 
 	findByEmail(email: string) {
 		return this.prisma.user.findUnique({ where: { email } });
 	}
 
 	create(input: CreateUserInput) {
+        this.eventEmitter.emit('user.registered',input);
 		return this.prisma.user.create({
 			data: {
 				email: input.email,
