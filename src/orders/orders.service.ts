289ec4e0387c32ea0a27eb0e4d28ddbd5abb62fe
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2026-03-26 11:43:20 +0100

    feat(orders): ajout de l'émission d'événements métier lors de la création d'une commande

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { BusinessEventPayloadDto } from 'src/events/business-event-payload.dto';
+import { TriggerType } from '@prisma/client';
 @Injectable()
 export class OrdersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService, private readonly eventEmitter: EventEmitter2,) {}
 
   async create(dto: CreateOrderDto, userId: string) {
     const order = await this.prisma.order.create({
@@ -13,6 +15,16 @@ export class OrdersService {
         status: 'created',
         userId,
       },
+    });
+
+        // Émet l'événement métier pour déclencher les workflows.
+    this.eventEmitter.emit(TriggerType.ORDER_CREATED, {
+      eventType: TriggerType.ORDER_CREATED,
+      data: {
+        orderId: order.id,
+        amount: order.amount,
+        userId: order.userId,
+      } as BusinessEventPayloadDto,
     });
 
     return order;
